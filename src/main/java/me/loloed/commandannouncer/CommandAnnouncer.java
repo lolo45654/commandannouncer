cddda052f716e7ee2344e6e58f6fd956c86bc470
loloed <100manolo@web.de>
2024-06-15 23:18:49 +0200

    add /announcerworking command

--- src/main/java/me/loloed/commandannouncer/CommandAnnouncer.java
+++ src/main/java/me/loloed/commandannouncer/CommandAnnouncer.java
@@ -2,18 +2,22 @@ package me.loloed.commandannouncer;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class CommandAnnouncer extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginCommand("announcerworking").setExecutor(this);
     }
 
     @EventHandler
@@ -31,5 +35,11 @@ public final class CommandAnnouncer extends JavaPlugin implements Listener {
         for (Player oPlayer : Bukkit.getOnlinePlayers()) {
             oPlayer.sendMessage(Component.text("Console has issued: " + command));
         }
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        sender.sendMessage(Component.text("Yes, I am active."));
+        return true;
     }
 }
